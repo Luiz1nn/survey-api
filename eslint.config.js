a8cf9343856de40abf1c69eda1fdc8891b839067
Luiz1nn <luisfernandobr2003@gmail.com>
2025-02-27 02:09:23 -0300

    🔧 ci: generate eslint and create rules

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: globals.node },
+    ignores: ['node_modules'],
+    rules: {
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-duplicate-imports': 'error',
+      'no-eval': 'error',
+      eqeqeq: 'error',
+      'no-else-return': 'error',
+      'no-useless-return': 'error'
+    }
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'error'
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
+]
